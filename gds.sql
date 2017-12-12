fca4d2ea9678c52bbdd7bcec804be8b049ac4d84
lizhengjie <513598006@qq.com>
2017-12-12 17:57:41 +0800

    sql

--- gds.sql
+++ gds.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50634
 File Encoding         : 65001
 
-Date: 2017-12-11 17:22:16
+Date: 2017-12-12 17:52:47
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -35,8 +35,8 @@ CREATE TABLE `gds_club` (
 -- ----------------------------
 -- Records of gds_club
 -- ----------------------------
-INSERT INTO `gds_club` VALUES ('1', 'IT文化', null, '计算机学院', null, '0', null, null, null);
-INSERT INTO `gds_club` VALUES ('2', '爱心公益社', null, '外国语学院', null, '0', null, null, null);
+INSERT INTO `gds_club` VALUES ('1', 'IT社团', null, '计算机学院', null, '0', null, null, null);
+INSERT INTO `gds_club` VALUES ('2', '爱心公益社团', null, '外国语学院', null, '0', null, null, null);
 
 -- ----------------------------
 -- Table structure for gds_dict
@@ -85,23 +85,29 @@ CREATE TABLE `gds_student` (
   `is_admin` int(1) DEFAULT '0' COMMENT '是否管理员',
   `is_disabled` int(1) DEFAULT '0' COMMENT '是否禁用',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='学生表\r\n';
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COMMENT='学生表\r\n';
 
 -- ----------------------------
 -- Records of gds_student
 -- ----------------------------
-INSERT INTO `gds_student` VALUES ('1', '666666666', '超级大BOSS', '123456', 'admin', '513598006@qq.com', '2017-11-30 11:39:10', '127.0.0.1', null, '127.0.0.1', '7777777777', '123456', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('2', '1232136', '张三', '31241', 'zxc', '1234@qq.com', '2017-12-01 11:39:10', '127.0.0.1', null, '127.0.0.1', '7777777777', '123456', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('3', '1232136', '张三丰', '666', 'zxxxxc', '123asd4@qq.com', '2017-05-30 11:39:10', '127.0.0.1', null, '127.0.0.1', '7777777777', '4534534', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('4', '1232136', '老王', '322323', 'wangwu', '12ewqe34@qq.com', '2017-01-30 11:39:10', '127.0.0.1', null, '127.0.0.1', '7777777777', '122343456', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('5', '1232136', '李白', 'sdfsdf', 'lisi', '1234wer@qq.com', null, '127.0.0.1', null, '127.0.0.1', '7777777777', '345', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('6', '1232136', '赵晓晓', '123123', 'zhaoliu', '12sdfs34@qq.com', null, '127.0.0.1', null, '127.0.0.1', '7777777777', '123234456', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('7', '1232136', '鲁班', 'hhhe', 'asf', '123wqeqwgfd4@qq.com', null, '127.0.0.1', null, '127.0.0.1', '7777777777', '231', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('8', '1232136', 'jery', '999', 'zwerwerxc', '1234@qq.com', null, '127.0.0.1', null, '127.0.0.1', '7777777777', '1234412356', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('9', '1232136', 'tom', '6666', 'zxdfgfdc', '123asdasd4@qq.com', '2017-11-30 11:39:10', '127.0.0.1', null, '127.0.0.1', '7777777777', '1234325', null, null, '0', '0', '0', '0');
-INSERT INTO `gds_student` VALUES ('10', '324234', '张小三', 'sdfsdfsd', 'future', 'zhangxiaosan@163.com', null, null, null, null, null, '234234', null, null, null, null, null, null);
-INSERT INTO `gds_student` VALUES ('11', '324234', '李小四', '123456', 'lixiaosi', 'lixiaosi@163.com', null, null, null, null, null, '2348732', '艺术学院', null, null, null, null, null);
+INSERT INTO `gds_student` VALUES ('1', '666666666', '超级大BOSS', '123456', 'admin', '513598006@qq.com', '2017-11-30 11:39:10', '127.0.0.1', null, '127.0.0.1', 'zxc', '123456', '计算机学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('2', '123213698', '张三', '31241', 'zxc', '1234@qq.com', '2017-12-01 11:39:10', '127.0.0.1', null, '127.0.0.1', 'wewsd', '123456', '外国语学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('3', '123782136', '张三丰', '666', 'zxxxxc', '123asd4@qq.com', '2017-05-30 11:39:10', '127.0.0.1', null, '127.0.0.1', 'qaq', '4534534', '计算机学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('4', '123213986', '老王', '322323', 'wangwu', '12ewqe34@qq.com', '2017-01-30 11:39:10', '127.0.0.1', null, '127.0.0.1', '909232', '122343456', '艺术学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('5', '123452136', '李白', 'sdfsdf', 'lisi', '1234wer@qq.com', null, '127.0.0.1', null, '127.0.0.1', '7777777777', '345', '艺术学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('6', '123265136', '赵晓晓', '123123', 'zhaoliu', '12sdfs34@qq.com', null, '127.0.0.1', null, '127.0.0.1', 'future', '123234456', '计算机学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('7', '123342136', '鲁班', 'hhhe', 'asf', '123wqeqwgfd4@qq.com', null, '127.0.0.1', null, '127.0.0.1', 'hhha', '231', '艺术学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('8', '54654745', 'jery', '999', 'zwerwerxc', '1234@qq.com', null, '127.0.0.1', null, '127.0.0.1', 'xixixi', '1234412356', '计算机学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('9', '1223232136', 'tom', '6666', 'zxdfgfdc', '123asdasd4@qq.com', '2017-11-30 11:39:10', '127.0.0.1', null, '127.0.0.1', '89bwye2', '1234325', '外国语学院', null, '0', '0', '0', '0');
+INSERT INTO `gds_student` VALUES ('10', '4443453643', '张小三', 'sdfsdfsd', 'zhangxiaosan12', 'zhangxiaosan@163.com', null, null, null, null, null, '234234', '外国语学院', null, null, null, null, null);
+INSERT INTO `gds_student` VALUES ('11', '32427856734', '李小四', '123456', 'lixiaosi', 'lixiaosi@163.com', null, null, null, null, null, '2348732', '艺术学院', null, null, null, null, null);
 INSERT INTO `gds_student` VALUES ('12', '9876543555', '王小五', '2143214', 'future2', 'wangxiaowu@qq.com', null, null, null, null, null, '12312412', '体育学院', null, null, null, null, null);
+INSERT INTO `gds_student` VALUES ('14', '1734782', '笑笑', 'xiaoixao1', 'xiaoxiao1', '7774@163.com', null, null, null, null, null, '666', '计算机学院', null, null, null, null, null);
+INSERT INTO `gds_student` VALUES ('15', '1776782', '笑笑', 'xiaoixao2', 'xiaoxiao2', '', null, null, null, null, null, '666', '计算机学院', null, null, null, null, null);
+INSERT INTO `gds_student` VALUES ('16', '1734634782', '笑笑', 'xiaoixao3', 'xiaoxiaoxiao3', '777@163.com', null, null, null, null, null, '666', '计算机学院', null, null, null, null, null);
+INSERT INTO `gds_student` VALUES ('17', '145678723', '超人', 'superman1', 'superman', '555@111.com', null, null, null, null, null, '158222222222', '艺术学院', null, null, null, null, null);
+INSERT INTO `gds_student` VALUES ('18', '15283676432', '超人', 'superman2', 'superman', '123@321.com', null, null, null, null, null, '123321', '计算机学院', null, null, null, null, null);
+INSERT INTO `gds_student` VALUES ('19', '299388477', '王小晨', 'wangdeng', 'wangdeng', 'wangxiaodeng@qq.com', '2017-12-12 10:33:59', null, null, null, null, '909898787', '计算机学院', null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for gds_stu_relation
@@ -109,14 +115,16 @@ INSERT INTO `gds_student` VALUES ('12', '9876543555', '王小五', '2143214', 'f
 DROP TABLE IF EXISTS `gds_stu_relation`;
 CREATE TABLE `gds_stu_relation` (
   `id` int(100) NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) DEFAULT NULL COMMENT '学生id',
+  `stu_num` varchar(20) DEFAULT NULL COMMENT '学生id',
   `dict_club_id` int(10) DEFAULT NULL COMMENT '所属社团id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of gds_stu_relation
 -- ----------------------------
+INSERT INTO `gds_stu_relation` VALUES ('1', '1652323723', '1');
+INSERT INTO `gds_stu_relation` VALUES ('2', '299388477', '1');
 
 -- ----------------------------
 -- Table structure for t_user
